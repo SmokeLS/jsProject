21d186f6deeb254caa24876cacb17b81193b44b3
Andrey <smokels1998@gmail.com>
2020-09-20 13:39:17 +0300

    modal update

--- js/script.js
+++ js/script.js
@@ -83,4 +83,75 @@ window.addEventListener("DOMContentLoaded",(event)=>{
     }
 
     setClock(".timer",deadline);
+    
+    //modal
+
+    const modal = document.querySelector(".modal"),
+          btnModal = document.querySelectorAll("[data-modal]"),
+          btnCloseModal = document.querySelectorAll("[data-close]");
+
+    btnModal.forEach(btn=> {
+        btn.addEventListener("click", ()=>{
+            modal.classList.add("show");
+            clearInterval(timerId);
+        });
+    });
+
+    btnCloseModal.forEach(btn=> {
+        btn.addEventListener("click", ()=> {
+            modal.classList.remove("show");
+        });
+    });
+
+    window.addEventListener("keyup", (event)=>{
+        if (event.key == "Escape" && modal.classList.contains("show")){
+            modal.classList.remove("show");
+        }
+    });
+
+    modal.addEventListener("click", function (event) {
+        const target = event.target;
+        if(target && target === this){
+            modal.classList.remove("show");
+        }
+    });
+
+    // const timerId = setInterval(()=>{
+    //     modal.classList.add("show");
+    // },5000);
+
+    window.addEventListener("scroll", (event)=>{
+        if (document.documentElement.clientHeight + window.pageYOffset == document.documentElement.scrollHeight){
+            modal.classList.add("show");modal.classList.add("show");
+        }
+    });
+
+    // class
+    const container = document.querySelector(".container");
+    class Menu{
+        constructor(src, alt, title, descr, price, parent){
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = parent;
+        }
+        render(){
+            let div = document.createElement("div");
+            div.innerHTML = `
+                <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>`;
+            document.querySelector(this.parent).append(div);
+        }
+    }
+    new Menu("img/tabs/post.jpg","post",'Меню "Постное"','Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.', 430, ".menu__field .container" ).render();
 });
